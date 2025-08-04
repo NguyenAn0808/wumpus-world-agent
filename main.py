60d6d53419fb1fc0facde001e56f276c97649538
NguyenAn0808 <23127148@student.hcmus.edu.vn>
2025-08-04 20:11:45 +0700

    Change DPLL

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from gui import *
 from gui.game_app.game_app import GameApp
 
 def main():
-    # game = GamePlay(display_callback = display_world)
-    
-    # game.run_console()
+    game = GamePlay(display_callback = display_world)
+    game.run_console()
 
     # GUI
     # game = GameApp()
